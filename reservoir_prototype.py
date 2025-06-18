06729c87ef0966b76ea6cc22b9760ec081175eb9
illarionov <alexey.illarionov@freecurve.com>
2025-06-18 18:09:54 +0300

    sendi box size

--- reservoir_prototype.py
+++ reservoir_prototype.py
@@ -5,10 +5,11 @@ import time
 from config_prototype import yaml_to_object
 from mpi_helper import MessageType, CtrlMsg, sync_process
 
+
 class TrrReader:
     def __init__(self, trr, gro):
         # Load the trajectory with the topology file
-        traj = md.load_trr(trr, top = gro)
+        traj = md.load_trr(trr, top=gro)
         # Number of frames
         print("Number of frames:", traj.n_frames)
         # Box size (unit cell vectors)
@@ -16,39 +17,48 @@ class TrrReader:
         # Shape of coordinates
         print("Coordinates shape:", traj.xyz.shape)
         # coordinates of coordinates
-        print(f"Coordinates: type={type(traj.xyz[0])} shape={traj.xyz[0].shape} data_type={traj.xyz[0].dtype}" )
+        print(
+            f"Coordinates: type={type(traj.xyz[0])} shape={traj.xyz[0].shape} data_type={traj.xyz[0].dtype}"
+        )
         self.traj = traj
-    def get_frame_coords(self,frame_index):
-        if 0<= frame_index and frame_index < self.traj.n_frames:
-            return self.traj.xyz[frame_index]
-        raise ValueError(f'frame_index beyond the range [0,{self.traj.n_frames})')
+
+    def get_frame_coords(self, frame_index):
+        if 0 <= frame_index and frame_index < self.traj.n_frames:
+            return self.traj.xyz[frame_index], self.traj.unitcell_vectors[frame_index]
+        raise ValueError(f"frame_index beyond the range [0,{self.traj.n_frames})")
+
     def get_num_frames(self):
         return self.traj.n_frames
 
 
 def main():
-    #config = ConfigPrototype()
+    # config = ConfigPrototype()
     config = yaml_to_object()
     sending_interval = config.reservoir_sending_interval
     num_iter = config.reservoir_num_iter
-
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
-    partner_rank = sync_process(config.reservoir_lambda_values, 'S', comm)
+    partner_rank = sync_process(config.reservoir_lambda_values, "S", comm)
     print(f"Sender with rank {rank} started")
-    if partner_rank != -1: 
+    if partner_rank != -1:
         msg = np.array([CtrlMsg.eStart.value], dtype=np.int32)
         comm.Send(msg, dest=partner_rank, tag=MessageType.eCtrlMsg.value)
-        trr_reader = TrrReader(config.reservoir_trr_file,config.reservoir_gro_file)
+        trr_reader,box = TrrReader(config.reservoir_trr_file, config.reservoir_gro_file)
         for i in range(num_iter):
             time.sleep(config.reservoir_sending_interval)  # Small delay between sends
-            data = trr_reader.get_frame_coords(np.random.randint(0, trr_reader.get_num_frames()))
-            print(f"Reservoir with rank {rank} sending data with shape {data.shape} (count: {i + 1}/{num_iter})")
+            data = trr_reader.get_frame_coords(
+                np.random.randint(0, trr_reader.get_num_frames())
+            )
+            print(
+                f"Reservoir with rank {rank} sending data with shape {data.shape} (count: {i + 1}/{num_iter})"
+            )
+            comm.Send(box, dest=partner_rank, tag=MessageType.eDataMesg.value)
             comm.Send(data, dest=partner_rank, tag=MessageType.eDataMesg.value)
 
         time.sleep(sending_interval)
         msg = np.array([CtrlMsg.eStart.value], dtype=np.int32)
         comm.Send(msg, dest=partner_rank, tag=MessageType.eCtrlMsg.value)
     print(f"Sender with rank {rank} finished")
+
 
 main()
